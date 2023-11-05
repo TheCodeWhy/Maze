3857dd0683cf24a90519dd5525fbf6749c396148
code why <136911594+TheCodeWhy@users.noreply.github.com>
2023-11-05 02:28:34 +0100

    icons click maze

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function main() {
 
+  const maze = document.getElementById("maze");
+  maze.addEventListener("click", function() {
+    window.location.href = "https://thecodewhy.github.io/Maze/";
+  });
+  const mathclimb = document.getElementById("mathclimb");
+  mathclimb.addEventListener("click", function() {
+    window.location.href = "https://thecodewhy.github.io/MathClimb/";
+  });
+
   const package = data()
   //var img = document.getElementById("scsh");
   var title = document.getElementById("title");
